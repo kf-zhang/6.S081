18efd96e4cee3280d827e03267cf13b652c7cd23
kf-zhang <1718121737@qq.com>
2022-01-28 08:24:46 +0000

    finish lab lock

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,25 +18,73 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct page{
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+};
+
+struct page freelists[NCPU];
+
+void
+init_list(struct page* p,uint64 startpa,uint64 endpa){
+
+    if(((uint64)startpa % PGSIZE) != 0 || (char*)startpa < end || (uint64)startpa >= PHYSTOP)
+        panic("init_list");
+    if(((uint64)endpa % PGSIZE) != 0 || (char*)endpa < end || (uint64)endpa >= PHYSTOP)
+        panic("init_list");
+
+    
+    struct run* r;
+    push_off();
+    acquire(&p->lock);
+    p->freelist = 0;
+    for(uint64 pa = startpa;pa<endpa;pa+=PGSIZE){
+        r = (struct run*)pa;
+        r->next = p->freelist;
+        p->freelist = r;
+    }
+    release(&p->lock);
+    pop_off();
+}
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+    uint64 startpa = PGROUNDUP((uint64)end );
+    uint64 endpa   = PGROUNDDOWN( (uint64)PHYSTOP );
+    uint64 span = PGROUNDDOWN( (endpa -startpa)/NCPU );
+
+    char name[6]="kmem0";
+    // printf("span:%p endpa:%p startpa:%p\n",span,endpa,startpa);
+    //init lock per CPU
+    for(int i=0;i<NCPU;i++){
+        // printf("i:%d startpa:%p endpa:%p end:%p\n",i,i*span+startpa,(i+1)*span+startpa,end);
+        name[4] = '0' + i;
+        initlock(&freelists[i].lock,name);
+        init_list(freelists+i,startpa+i*span,startpa+(i+1)*span);
+    }
+
 }
 
+
 void
-freerange(void *pa_start, void *pa_end)
-{
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+idx_kfree(int idx,void *pa){
+  struct run *r;
+  struct page*kmem = &freelists[idx];
+  r = (struct run*)pa;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP || idx < 0 || idx >= NCPU)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 0, PGSIZE);
+
+  push_off();  
+  acquire(&kmem->lock);
+  r->next = kmem->freelist;
+  kmem->freelist = r;
+  release(&kmem->lock);
+  pop_off();
 }
 
 // Free the page of physical memory pointed at by v,
@@ -46,37 +94,53 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
-
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
-
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
-
-  r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  int idx = cpuid();
+  idx_kfree(idx,pa);
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
+void*
+idx_kalloc(int idx)
+{
+  if(idx<0||idx>=NCPU)
+    return 0;
+
+  struct run *r;
+  struct page *kmem = &freelists[idx];
+  push_off();
+  acquire(&kmem->lock);
+  r = kmem->freelist;
+  if(r){
+    kmem->freelist = r->next;
+    release(&kmem->lock);
+    pop_off();
+    memset((char*)r, 0, PGSIZE);
+    return (void*)r;
+  }
+  else{
+   release(&kmem->lock); 
+   pop_off();
+   return 0;
+  }
+}
+
+
 void *
 kalloc(void)
 {
-  struct run *r;
-
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
-
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  int idx = cpuid();
+  // printf("cpuid:%d\n",idx);
+  void* p = 0;
+  for(int i=0;i<NCPU;i++){
+    p = idx_kalloc(idx);
+    if(p)
+        return p;
+    else{
+        // printf("cpu %d out of memory\n",idx);
+        idx = (idx+1)%NCPU;
+      }
+  }
+  return 0;
 }
